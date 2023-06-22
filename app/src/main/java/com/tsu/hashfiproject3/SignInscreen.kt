fcb45d0bb9b4bc7b9cd5b4943578fb4b326a714f
Khashfi Khikmat <hashfiputrazah@gmail.com>
2023-06-22 19:20:16 +0700

    Initial commit

---
+++ app/src/main/java/com/tsu/hashfiproject3/SignInscreen.kt
@@ -0,0 +1,11 @@
+package com.tsu.hashfiproject3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_screen)
+    }
+}
